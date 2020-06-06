6ddffba3cf03694668890a08074938afc6d64492
blksnk <art.jnveigel@gmail.com>
2020-06-06 17:12:31 +0200

    :construction:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv")
 const path = require("path")
 const express = require("express")
+const morgan = require("morgan")
 const cors = require("cors")
 const { createClient } = require("./database/client.js")
 
@@ -12,6 +13,7 @@ const { PORT, DATABASE_URL } = process.env
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(morgan('tiny'))
 
 createClient(DATABASE_URL)
 
@@ -23,5 +25,5 @@ app.use('/database', databaseRouter)
 
 
 app.listen(PORT, () => {
-  console.log('server started on local port ', PORT)
+  console.log('server started on local port', PORT)
 })
